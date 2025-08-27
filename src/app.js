24fee6ef5ffbf6a49677e29ddc3eefd0089ed4b4
anastasy_klimanova <klimanova_anastasy@mail.ru>
2025-08-27 22:56:39 +0300

    second task done

--- src/app.js
+++ src/app.js
@@ -6,3 +6,9 @@ const characters = [
 ];
 
 const alive = characters.filter(item => item.health > 0);
+
+console.log(alive); // добавлен вывод в консоль для использования переменной alive
+// ответ терминала: npm run lint
+// > defender-game@1.0.0 lint
+// > eslint .
+
